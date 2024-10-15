1b511ee0a695d7e497ab2e89039bee8a3bbbfbb3
Solanki Yogesh <yogesh.s@arccus.in>
2024-10-15 15:41:36 +0530

    feat: splash screen implementation done

--- lib/screens/splash_screen/splash_screen.dart
+++ lib/screens/splash_screen/splash_screen.dart
@@ -1,15 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 
-class SplashScreen extends StatelessWidget {
+class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    FlutterNativeSplash.remove();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('SplashScreen'),
-      ),
-      body: Center(
-        child: Text('SplashScreen'),
+    return Container(
+      alignment: Alignment.center,
+      child: Text(
+        "Hello world",
+        style: TextStyle(color: Colors.white),
       ),
     );
   }
